dc682892973cc600343eb3264a305c17310545fa
unknown <denilavran@yandex.ru>
2022-02-23 22:35:28 +0300

    forth version

--- photo_to_disk.py
+++ photo_to_disk.py
@@ -5,8 +5,8 @@ import time
 import json
 
 
-ya_token = "AQAAAABVJsmLAADLW3Qbu3zpx0GhgLQhhGY94qs"
-vk_token = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
+ya_token = ""
+vk_token = ""
 
 ya_disk_path = "fromvkphotos/"
 os.mkdir('photos')
